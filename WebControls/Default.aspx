b1efa11f23c0b764e3fc4d0be1d70cdd44ff3db9
Usman Tarfa <utarfa@fortegra.com>
2015-11-20 14:27:52 -0500

    How implement and use WebControls

---
+++ WebControls/Default.aspx
@@ -0,0 +1,45 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="WebControls.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:CheckBox ID="SSD1" Text="SSD?" runat="server" />
+        <asp:CheckBox ID="SixteenGB" Text="16GB?" runat="server" />
+        <asp:CheckBox ID="DaulMonitor" Text="Dual Monitor?" runat="server" />
+        <hr/>
+        <asp:RadioButton ID="MaleRadioButton" GroupName="Sex" Text="Male" Checked="True" runat="server" />
+        <asp:RadioButton ID="FemaleRadioButton" GroupName="Sex" Text="Female" runat="server" />
+        <hr/>
+        <asp:CheckBoxList ID="CheckBoxList1" runat="server">
+             <asp:ListItem Text="Power Windows" Value="Feature1"></asp:ListItem>
+             <asp:ListItem Text="Power Seats" Value="Feature2"></asp:ListItem>
+             <asp:ListItem Text="For Lights" Value="Feature3"></asp:ListItem>
+             <asp:ListItem Text="Winter Packag" Value="Feature4"></asp:ListItem>
+        </asp:CheckBoxList>
+        <hr/>
+        <asp:RadioButtonList ID="RadioButtonList1" runat="server">
+             <asp:ListItem Text="Under 30" Value="Young" Selected="True"></asp:ListItem>
+             <asp:ListItem Text="30 - 60" Value="Middle-Age" ></asp:ListItem>
+             <asp:ListItem Text="Over 60" Value="Old-Age"></asp:ListItem>
+        </asp:RadioButtonList>
+        <hr/>
+
+        <asp:DropDownList ID="DropDownList1" runat="server">
+             <asp:ListItem Text="Item 1" Value="1" />
+             <asp:ListItem Text="Item 2" Value="2" />
+             <asp:ListItem Text="Item 3" Value="3" />
+             <asp:ListItem Text="Item 4" Value="4" />
+        </asp:DropDownList>
+        <hr/>
+        <asp:Button ID="SubmitButton" runat="server" Text="Submit" OnClick="SubmitButton_Click" />
+        <asp:Label ID="Message" runat="server" Text=""></asp:Label>
+    </div>
+    </form>
+</body>
+</html>
---
+++ WebControls/Default.aspx.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebControls
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void SubmitButton_Click(object sender, EventArgs e)
+        {
+            /*
+             * For CheckBox control, you assign the variable to boolean 
+             * and use a tenery operator for your condition to get
+             * the value the user selects.
+             */
+
+            bool ssd = SSD1.Checked;
+            bool ram = SixteenGB.Checked;
+            bool dual = DaulMonitor.Checked;
+
+            Message.Text = "You selcted: ";
+            Message.Text += ssd ? " SSD Drive, " : "";
+            Message.Text += ram ? " 16GB RAM, " : "";
+            Message.Text += dual ? " Dual Monitor, " : "";
+
+            /*
+             * For CheckBoxList use a foreach loop to iterate through 
+             * the items listCollection
+             * 
+             */
+            foreach (ListItem li in CheckBoxList1.Items)
+            {
+                if (li.Selected)
+                {
+                    Message.Text += li.Value.ToString();
+                }
+            }
+
+
+            /*
+             * 
+             */
+            Message.Text += "</br> The Customer is ";
+            Message.Text += MaleRadioButton.Checked ? "Male" : "Female";
+
+            /*
+             * RadioButtonList:
+             * Similar how to iterate through CheckBoxList 
+             */
+            Message.Text += "</br> Your age group is considered as ";
+            foreach (ListItem li in RadioButtonList1.Items)
+            {
+                if (li.Selected)
+                {
+                    Message.Text += li.Value.ToString() + ". ";
+                }
+            }
+
+            /*
+             * 
+             */
+            Message.Text += "You selected Item ";
+            Message.Text += DropDownList1.SelectedValue.ToString();
+        }
+    }
+}
