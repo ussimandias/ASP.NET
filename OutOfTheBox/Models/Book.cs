c9b3908bcd3d2f1e01490ded38ea8650fb92e805
Usman Tarfa <utarfa@fortegra.com>
2015-11-30 16:29:55 -0500

    Model - Data Entry Using Entity Framework

---
+++ OutOfTheBox/Models/Book.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace OutOfTheBox.Models
+{
+    public class Book
+    {
+	    public int ID { get; set; }
+        
+	    public string Name { get; set; }
+
+        [Display(Name = "Pub Date")]
+        [DataType(DataType.Date)]
+	    public DateTime PubDate { get; set; }
+
+	    public decimal Price { get; set; }
+    }
+}
--- OutOfTheBox/Models/IdentityModels.cs
+++ OutOfTheBox/Models/IdentityModels.cs
@@ -29,5 +29,7 @@ namespace OutOfTheBox.Models
         {
             return new ApplicationDbContext();
         }
+
+        public System.Data.Entity.DbSet<OutOfTheBox.Models.Book> Books { get; set; }
     }
 }
