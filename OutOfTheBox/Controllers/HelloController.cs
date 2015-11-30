3d9a9916be19e65c380a921cb3ee427e0e211b83
Usman Tarfa <utarfa@fortegra.com>
2015-11-30 11:45:48 -0500

    Controller Convention

---
+++ OutOfTheBox/Controllers/HelloController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OutOfTheBox.Controllers
+{
+    public class HelloController : Controller
+    {
+        // GET: Hello
+        public string Index()
+        {
+            return "This is an <b>HTML</b> string..";
+        }
+
+        public string HelloWorld()
+        {
+            return "Hello World!.. ";
+        }
+    }
+}
