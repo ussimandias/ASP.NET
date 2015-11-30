0f95c2a5bf39393aa3ce0031ae9ec9849e902b43
Usman Tarfa <utarfa@fortegra.com>
2015-11-30 09:43:18 -0500

    MVC Out of the Box

---
+++ OutOfTheBox/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OutOfTheBox.Startup))]
+namespace OutOfTheBox
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
