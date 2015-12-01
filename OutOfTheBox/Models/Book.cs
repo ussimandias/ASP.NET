a59f730de25a648f3f29c36ac941933f5f32fc2d
Usman Tarfa <utarfa@fortegra.com>
2015-12-01 14:19:33 -0500

    Validation - Data Attributes

--- OutOfTheBox/Models/Book.cs
+++ OutOfTheBox/Models/Book.cs
@@ -9,14 +9,20 @@ namespace OutOfTheBox.Models
     public class Book
     {
 	    public int ID { get; set; }
-        
+
+        [Required(AllowEmptyStrings =false, ErrorMessage = "You must enter a name")]
+        [StringLength( 60, MinimumLength =3, ErrorMessage ="Must be between 3 and 60")]
 	    public string Name { get; set; }
 
         [Display(Name = "Pub Date")]
         [DataType(DataType.Date)]
 	    public DateTime PubDate { get; set; }
 
+        [DataType( DataType.Currency)]
+        [Range(1,100, ErrorMessage = "Must enter $1 and $100"  )]
 	    public decimal Price { get; set; }
+
+        [Required (AllowEmptyStrings = false, ErrorMessage = "You must enter a genre")]
         public string  Genre { get; set; }
 
     }
