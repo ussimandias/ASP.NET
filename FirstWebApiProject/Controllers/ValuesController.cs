27d0fb97515dcebf7bb965ec4f718599c469e0e0
Usman Tarfa <utarfa@fortegra.com>
2015-12-02 16:06:56 -0500

    Post/Get methods - Fiddler

--- FirstWebApiProject/Controllers/ValuesController.cs
+++ FirstWebApiProject/Controllers/ValuesController.cs
@@ -4,37 +4,51 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using FirstWebApiProject.Models;
 
 namespace FirstWebApiProject.Controllers
 {
     [Authorize]
-    public class ValuesController : ApiController
+    public class ProductController : ApiController
     {
         // GET api/values
-        public IEnumerable<string> Get()
+        public IEnumerable<Product> Get()
         {
-            return new string[] { "value1", "value2" };
+            //return new string[] { "value1", "value2" };
+            return Models.Product.Products;
         }
 
         // GET api/values/5
-        public string Get(int id)
+        public Product Get(int id)
         {
-            return "value";
+            return Models.Product.Products.FirstOrDefault(p => p.ID == id);
         }
 
         // POST api/values
-        public void Post([FromBody]string value)
+        public HttpResponseMessage Post([FromBody]Product value)
         {
+            Product.Products.Add(value);
+
+            var response = new HttpResponseMessage(HttpStatusCode.Created);
+            response.Headers.Location = new Uri(Url.Link( "DefaultApi", new {id = value.ID}));
+            return response;
         }
 
         // PUT api/values/5
-        public void Put(int id, [FromBody]string value)
+        public void Put(int id, [FromBody]Product value)
         {
+            var thisProduct = Product.Products.FirstOrDefault(p => p.ID == id);
+            if (thisProduct != null)
+            {
+                thisProduct.Name = value.Name;
+                thisProduct.Price = value.Price;
+            }
         }
 
         // DELETE api/values/5
         public void Delete(int id)
         {
+            Product.Products.Remove(Product.Products.First(p => p.ID == id));
         }
     }
 }
---
+++ FirstWebApiProject/Models/Product.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace FirstWebApiProject.Models
+{
+    [DataContract]
+    public class Product
+    {
+        [DataMember]
+        public int ID { get; set; }
+
+        [DataMember]
+        public string Name { get; set; }
+
+        [DataMember]
+        public decimal Price { get; set; }
+
+        public static readonly List<Product> Products = new List<Product>
+        {
+            new Product{ID = 1, Name ="Playstation Bundle", Price = 299m},
+            new Product{ID = 2, Name ="LifeProof iPhone 6 Case", Price = 79m},
+            new Product{ID = 3, Name ="New Balance Trainers", Price = 89m},
+            new Product{ID = 4, Name ="StarWars BattleFront PS4", Price = 59.99m},
+
+        };
+    }
+}
